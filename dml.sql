93ba2bf310b2421c447e17f9a89d225249720b21
yenight <30335276+yenight@users.noreply.github.com>
2019-06-30 00:20:08 +0800

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- 插入记录  
-
+insert into student (name, age, sex) values ("aaa", 10, "男");
 -- 修改记录  
-
+update student set name="aaaaa" where name="aaa" 
 --  删除记录  
-
+delete from student where age=18;
 -- 查询记录  
+select * from student where name="张三";
